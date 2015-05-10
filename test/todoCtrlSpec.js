a02db21d672884e7ef0f48623592eecd4e27c5c1
Tomiblanchard <tomiblanchard@gmail.com>
2015-05-09 19:03:47 -0600

    added tests

---
+++ test/todoCtrlSpec.js
@@ -0,0 +1,26 @@
+'use strict';
+
+describe('todo Controller', function () {
+
+  it('should not have any Todos at start', function() {
+
+  });
+
+  it('should not have an edited todo on start', function() {
+
+  });
+
+
+  it('should not have all todos completed', function() {
+
+  });
+
+
+  it('should count todo correctly', function() {
+
+  });
+
+
+
+
+});
